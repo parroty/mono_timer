3c57a7744721eea7e4fbb5d8c2a0278d4f0219d4
parroty <parroty00@gmail.com>
2015-02-01 09:54:22 +0900

    Replace integration test with capybara feature test

---
+++ test/features/timer_operation_test.rb
@@ -0,0 +1,22 @@
+require "test_helper"
+
+class TimerOperationTest < Capybara::Rails::TestCase
+  setup do
+    Timer.create!(start_time: DateTime.now, status: Timer::STATUS_ACTIVE, category: "Programming")
+  end
+
+  test "gets timer index page" do
+    visit '/timer'
+    assert_content page, "Mono Timer"
+  end
+
+  test "gets timer list page" do
+    visit '/timer/list'
+    assert_content page, "Programming"
+  end
+
+  test "gets new timer page" do
+    visit '/timer/new'
+    assert_content page, "New Timer"
+  end
+end
