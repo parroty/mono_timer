86c93446fe2418330bafcfed47bbca8e881498c7
parroty <parroty00@gmail.com>
2015-02-05 00:35:08 +0900

    Add exception logic for worker for retrying

--- lib/stop_timer_worker.rb
+++ lib/stop_timer_worker.rb
@@ -3,7 +3,9 @@ class StopTimerWorker
 
   def perform(timer_id)
     timer = Timer.find(timer_id)
-    if timer.remaining_seconds == 0
+    if timer.remaining_seconds > 0
+      raise "Timer(#{timer_id} is still running"
+    else
       timer.stop
       timer.save
     end
