0188f37761d3b38b418279ac14740dda54c9b844
parroty <parroty00@gmail.com>
2015-02-08 23:42:26 +0900

    Fix error handling of http response

--- app/services/pushover_notifier.rb
+++ app/services/pushover_notifier.rb
@@ -1,7 +1,7 @@
 class PushoverNotifier
   def notify(message, title = 'Mono Timer')
     response = Pushover.notification(message: message, title: title)
-    if response.class != Net::HTTPSuccess
+    unless response.kind_of? Net::HTTPSuccess
       raise "Sending notification failed with error: #{response.body}"
     end
   end
--- test/services/pushover_notifier_test.rb
+++ test/services/pushover_notifier_test.rb
@@ -2,33 +2,33 @@ require 'test_helper'
 
 class PushoverNotifierTest < ActiveSupport::TestCase
   test "sends message succeeds with default title" do
-    response = mock()
-    response.expects(:class).returns(Net::HTTPSuccess)
+    response = Net::HTTPOK.new(nil, 200, nil)
     Pushover.expects(:notification)
-      .with(message: "Sample Message", title: "Mono Timer").returns(response)
+      .with(message: "Sample Message", title: "Mono Timer")
+      .returns(response)
 
     PushoverNotifier.new.notify("Sample Message")
   end
 
   test "sends message succeeds with custom title" do
-    response = mock()
-    response.expects(:class).returns(Net::HTTPSuccess)
+    response = Net::HTTPOK.new(nil, 200, nil)
     Pushover.expects(:notification)
-      .with(message: "Sample Message", title: "Sample Title").returns(response)
+      .with(message: "Sample Message", title: "Sample Title")
+      .returns(response)
 
     PushoverNotifier.new.notify("Sample Message", "Sample Title")
   end
 
   test "send message fails" do
-    response = mock()
-    response.expects(:class).returns(Net::HTTPBadRequest)
-    response.expects(:body).returns("error")
+    response = Net::HTTPBadRequest.new(nil, 400, nil)
+    response.expects(:body).returns("error_message_body")
     Pushover.expects(:notification)
-      .with(message: "Sample Message", title: "Mono Timer").returns(response)
+      .with(message: "Sample Message", title: "Mono Timer")
+      .returns(response)
 
     error_message = assert_raises RuntimeError do
       PushoverNotifier.new.notify("Sample Message")
     end
-    assert_match "error", error_message.to_s
+    assert_match "error_message_body", error_message.to_s
   end
 end
