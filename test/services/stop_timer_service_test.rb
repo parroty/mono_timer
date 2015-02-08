aed78d4ea6b50ce4ddce13670c7d777a0d6a985d
parroty <parroty00@gmail.com>
2015-02-08 21:42:47 +0900

    Refactor tests for StopTimerServiceTest

--- test/services/stop_timer_service_test.rb
+++ test/services/stop_timer_service_test.rb
@@ -5,13 +5,17 @@ class StopTimerServiceTest < ActiveSupport::TestCase
     @timer = Timer.create(start_time: 5.minutes.ago, end_time: nil)
   end
 
-  test "creation of stop timer succeeds" do
+  test "create method invokes StopTimerWorker#perform_in with correct parameters" do
     StopTimerWorker.expects(:perform_in).with(25 * 60, @timer.id, true)
+
     StopTimerService.create(@timer.id)
   end
 
-  test "destroy of stop timer succeeds" do
-    # TODO: needs assertion with certain way of Sidekiq::ScheduledSet mocking.
+  test "destry method invokes delete for the corresponding entry in the Sidekiq::ScheduledSet" do
+    entry = stub(item: {"args" => [@timer.id]})
+    entry.expects(:delete)
+    Sidekiq::ScheduledSet.expects(:new).returns([entry])
+
     StopTimerService.destroy(@timer.id)
   end
 end
