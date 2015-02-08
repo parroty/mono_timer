5a43483a8148c5ec32239099a3d87f84502862bd
parroty <parroty00@gmail.com>
2015-02-08 19:25:27 +0900

    Temporarily specify fixed timezone for history page display

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,7 @@ module MonoTimer
     config.active_record.raise_in_transactional_callbacks = true
 
     config.autoload_paths += %W(#{config.root}/lib)
+
+    config.time_zone = 'Tokyo'
   end
 end
