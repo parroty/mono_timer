ba7359af05bb669d13f9394646fbbb657f624d43
parroty <parroty00@gmail.com>
2015-02-09 21:47:08 +0900

    Add basic authentication for sidekiq console

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,14 @@ Rails.application.routes.draw do
   post "timer/:id/stop" => 'timer#stop', as: :timer_stop
 
   require 'sidekiq/web'
+
+  user = ENV['SIDEKIQ_MANAGEMENT_USER']
+  pass = ENV['SIDEKIQ_MANAGEMENT_PASS']
+
+  if user != nil && pass != nil
+    Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+      username == user && password == user
+    end
+  end
   mount Sidekiq::Web => '/sidekiq'
 end
