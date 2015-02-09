3c7f1a0f9c07a0d74eecca2989c74a5406a3cf56
parroty <parroty00@gmail.com>
2015-02-09 21:20:10 +0900

    Update layout for mobile - specify viewport and remove extra columns

--- app/helpers/timer_helper.rb
+++ app/helpers/timer_helper.rb
@@ -2,4 +2,8 @@ module TimerHelper
   def seconds_to_timer_str(seconds)
     sprintf "%02d:%02d", (seconds / 60).floor, seconds % 60
   end
+
+  def to_display_time(datetime)
+    datetime.strftime("%Y/%m/%d %H:%M:%S")
+  end
 end
