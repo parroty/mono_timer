df10bbc434935319f5bc345671dec7d626554ae1
parroty <parroty00@gmail.com>
2015-03-01 22:21:16 +0900

    Make it reload initializer at dev-mode for not-erasing parameters

--- config/initializers/notifier.rb
+++ config/initializers/notifier.rb
@@ -1,10 +1,12 @@
-Notifier::Pushover.configure do |config|
-  config.user  = ENV['PUSHOVER_USER_KEY']
-  config.token = ENV['PUSHOVER_API_KEY']
-end
+Rails.application.config.to_prepare do
+  Notifier::Pushover.configure do |config|
+    config.user  = ENV['PUSHOVER_USER_KEY']
+    config.token = ENV['PUSHOVER_API_KEY']
+  end
 
-Notifier::Mailgun.configure do |config|
-  config.api_key = ENV["MAILGUN_API_KEY"]
-  config.domain  = ENV["MAILGUN_DOMAIN"]
-  config.address = ENV["MONO_TIMER_EMAIL"]
+  Notifier::Mailgun.configure do |config|
+    config.api_key = ENV["MAILGUN_API_KEY"]
+    config.domain  = ENV["MAILGUN_DOMAIN"]
+    config.address = ENV["MONO_TIMER_EMAIL"]
+  end
 end
